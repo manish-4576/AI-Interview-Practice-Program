60958ea9e289eaba9bba61b4d045532991792274
Manish <20manish20kumar20@gmail.com>
2026-02-15 15:46:15 +0530

    fix: use atlas

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use("/api/interview", interviewRoutes);
 
 // MongoDB Connection
-mongoose.connect("mongodb://127.0.0.1:27017/interviewDB")
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
